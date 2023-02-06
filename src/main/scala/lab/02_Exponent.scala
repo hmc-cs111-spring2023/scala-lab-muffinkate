dacdd1a67bd818e273b1181a6bedab63e5c70818
Kate Phillips <47925526+muffinkate@users.noreply.github.com>
2023-02-06 22:28:42 +0000

    worked on lab

--- src/main/scala/lab/02_Exponent.scala
+++ src/main/scala/lab/02_Exponent.scala
@@ -2,4 +2,7 @@
 /** 
   * Computes n^e
   */
-def expt(n: Int, e: Int): Int = -1
+def expt(n: Int, e: Int): Int = 
+  if e == 0 then 1
+  else if e == 1 then n
+  else n * expt(n, e-1)
--- src/main/scala/lab/03_Palindrome.scala
+++ src/main/scala/lab/03_Palindrome.scala
@@ -4,4 +4,8 @@
   *  Hint: the Scala collections API is your friend
   *  https://docs.scala-lang.org/overviews/scala-book/collections-methods.html
   */
-def isPalindrome(s: String): Boolean = throw new NotImplementedError
+def isPalindrome(s: String): Boolean = 
+  val s_list = s.toList
+  val s_rev = s.reverse
+  if s == s_rev then true
+  else false
